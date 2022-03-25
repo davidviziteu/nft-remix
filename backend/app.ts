b8d32b431697fe27560d6a489ab4922b1c372cdb
gabriel.bacauanu <gabyedy22@gmail.com>
2022-03-25 13:19:44 +0200

    Add interfaces example for both backend and frontend

--- backend/app.ts
+++ backend/app.ts
@@ -1,7 +1,20 @@
 import express from 'express';
+import {IBigInterface, ISubInterface} from "../interfaces/example";
 
 const app = express();
 const port = 3000;
+
+const usefulSubConstant: ISubInterface = {
+    subProperty1: 1,
+    subProperty2: "A test string",
+    subProperty3: "this can be any type of variable"
+};
+const usefulConstant: IBigInterface = {
+    property1: usefulSubConstant,
+    property2: []
+};
+
+console.log(usefulConstant?.property1?.subProperty2);
 
 app.listen(port, () => {
     console.log(`Timezones by location application is running on port ${port}.`);
